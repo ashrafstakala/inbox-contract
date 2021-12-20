66cdfda35d49b47f53df24e5c447ba2b64d94d21
ashrafstakala <ashrafstakala@gmail.com>
2021-12-21 00:18:32 +0200

    added 1st Mocha test case

--- compile.js
+++ compile.js
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts[':Inbox']; // to get only the bytecode
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // to get only the bytecode and ABI
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,20 @@
-// contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // constructor function / class
+const web3 = new Web3(ganache.provider()); // instance of web3. Ganache provider for local test eth network. Other providers for other networks
+
+class Car {
+  park() {
+    return 'stopped';
+  }
+  drive() {
+    return 'vroom';
+  }
+}
+
+describe('Car', () => {
+  it('can park', () => {
+    const car = new Car();
+    assert.equal(car.park(), 'stopped');
+  });
+});
