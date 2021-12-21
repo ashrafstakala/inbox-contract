57ee826c0abc4af7b4e08db1d55d1a549d092b15
ashrafstakala <ashrafstakala@gmail.com>
2021-12-21 15:28:31 +0200

    set up initial deploy code w/ infura

--- deploy.js
+++ deploy.js
@@ -1,1 +1,9 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'crack afford tattoo silver gadget unfair problem broccoli execute grit rich merge', // test Metamask wallet with test ETH
+  'https://rinkeby.infura.io/v3/b63698a4df7c4a2497d508687d5bd4dd' // Infura endpoint
+);
+const web3 = new Web3(provider);
