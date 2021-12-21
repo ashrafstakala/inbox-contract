a178238f719032f2a52a887903767e7199517315
ashrafstakala <ashrafstakala@gmail.com>
2021-12-21 10:20:05 +0200

    fetching accounts using web3 module

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,27 +3,43 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor function / class
 const web3 = new Web3(ganache.provider()); // instance of web3. Ganache provider for local test eth network. Other providers for other networks
 
-class Car {
-  park() {
-    return 'stopped';
-  }
-  drive() {
-    return 'vroom';
-  }
-}
-
-let car;
-
 beforeEach(() => {
-  car = new Car();
+  // Get a list of all accounts
+  web3.eth
+    .getAccounts() // Etherium module of Web3 library
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+
+  // Use one of those accounts to deploye the contract
 });
 
-describe('Car', () => {
-  it('can park', () => {
-    assert.equal(car.park(), 'stopped');
-  });
-
-  it('can drive', () => {
-    assert.equal(car.drive(), 'vroom');
-  });
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
 });
+
+////// Demo test
+// class Car {
+//   park() {
+//     return 'stopped';
+//   }
+//   drive() {
+//     return 'vroom';
+//   }
+// }
+
+// let car;
+
+// beforeEach(() => {
+//   car = new Car();
+// });
+
+// describe('Car', () => {
+//   it('can park', () => {
+//     assert.equal(car.park(), 'stopped');
+//   });
+
+//   it('can drive', () => {
+//     assert.equal(car.drive(), 'vroom');
+//   });
+// });
