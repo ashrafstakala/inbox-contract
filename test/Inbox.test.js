3c5d8c2130835ab319626c344c810018ba4b7598
ashrafstakala <ashrafstakala@gmail.com>
2021-12-21 11:26:00 +0200

    refactor with async await, import bytecode from compiler

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,20 +2,21 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor function / class
 const web3 = new Web3(ganache.provider()); // instance of web3. Ganache provider for local test eth network. Other providers for other networks
+const { interface, bytecode } = require('../complile');
 
-beforeEach(() => {
+let accounts;
+
+beforeEach(async () => {
   // Get a list of all accounts
-  web3.eth
-    .getAccounts() // Etherium module of Web3 library
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts(); // Etherium module of Web3 library
 
   // Use one of those accounts to deploye the contract
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
 });
 
 ////// Demo test
